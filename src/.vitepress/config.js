3ad54e39b15d7de82f5129b601f257e8cb12b6f3
Anon Ymous <ancientstraits@protonmail.com>
2026-01-21 19:28:35 -0500

    initial commit

---
+++ src/.vitepress/config.js
@@ -0,0 +1,9 @@
+import { defineConfig } from 'vitepress'
+
+export default defineConfig({
+    title: "Deltamod Modders' Guide",
+    description: "An in-development modders guide",
+    vite: {
+        configFile: "vite.config.js"
+    }
+})
