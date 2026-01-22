d09a44d21a08e52e862f9f48c89434f2b08031bf
Anon Ymous <ancientstraits@protonmail.com>
2026-01-21 23:46:24 -0500

    changed base for site

--- src/.vitepress/config.js
+++ src/.vitepress/config.js
@@ -3,6 +3,7 @@ import { defineConfig } from 'vitepress'
 export default defineConfig({
     title: "Deltamod Modders' Guide",
     description: "An in-development modders guide",
+    base: "/deltamodders-guide/",
     vite: {
         configFile: "vite.config.js"
     }
